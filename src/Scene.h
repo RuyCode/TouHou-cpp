b6e14982db490977fa19d518ebfc242f2934f878
NumPix <lecpix@vk.com>
2025-02-14 10:42:07 +0300

    Removed explicit default destructor definition

--- src/Scene.h
+++ src/Scene.h
@@ -5,8 +5,6 @@
 
 class Scene {
 public:
-    virtual ~Scene() = default;
-
     virtual void FixedUpdate(sf::RenderWindow& window, float fixedDeltaTime) = 0;
 
     virtual void Update(sf::RenderWindow& window, float deltaTime) = 0;
